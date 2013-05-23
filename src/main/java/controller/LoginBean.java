9689a3ea43aef98f3cd8210bffb4eb5cdff173f4
chandrasekhar4u <chandu624@gmail.com>
2013-05-23 19:33:05 +0530

    added templates for the screen. added servlet api in pom.

--- src/main/java/controller/LoginBean.java
+++ src/main/java/controller/LoginBean.java
@@ -3,11 +3,18 @@
  */
 package main.java.controller;
 
+import java.io.IOException;
+
 import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.myfaces.orchestra.conversation.ConversationBindingEvent;
 import org.apache.myfaces.orchestra.viewController.annotations.ViewController;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.web.client.HttpServerErrorException;
 
 /**
  * @author CHANDRA
@@ -66,6 +73,40 @@ public class LoginBean extends BaseController {
 	return password;
     }
 
+    public String actionLogOut(){
+	logOut();
+	return null;
+    }
+    
+    
+
+    /**
+     * 
+     */
+    public static void logOut() {
+
+	FacesContext fc=FacesContext.getCurrentInstance();
+	ExternalContext ec= fc.getExternalContext();
+	HttpServletRequest request= (HttpServletRequest) ec.getRequest();
+	HttpServletResponse response=(HttpServletResponse) ec.getResponse();
+	request.getSession(false).invalidate();
+
+	// re-direct to the application root e.g. /ProdisMoffatPlus
+	// Spring security will automatically forward to the login page as 
+	// the session has now been invalidated
+	try {
+	    String contextRoot = ec.getRequestContextPath();
+	    System.out.println( "redirecting to context root - " + contextRoot );
+	    response.sendRedirect( contextRoot );
+	}
+	catch ( IOException e ) {
+	    System.out.println( "exception redirecting to logon page - " + e.toString() );
+	}
+
+	// tell JSF the response is complete
+	fc.responseComplete();
+    }
+
     /**
      * @param password
      *            the password to set
@@ -75,16 +116,34 @@ public class LoginBean extends BaseController {
     }
 
     public String actionSubmit() {
-
+	FacesContext context=FacesContext.getCurrentInstance();
 	if (userName == null) {
-	    FacesContext.getCurrentInstance().addMessage("Message",
+	    context.addMessage("Message",
 		    new FacesMessage("Invalid User Name."));
+	    return null;
+
 	}
 	if (password == null) {
-	    FacesContext.getCurrentInstance().addMessage("Message",
+	    context.addMessage("Message",
 		    new FacesMessage("Invalid Password."));
+	    return null;
 
 	}
+	if(userName!=null){
+	    if(userName.compareToIgnoreCase("1234")!=0){
+		context.addMessage("Message",
+			    new FacesMessage("Invalid User Name."));
+		return null;
+	    }
+	}
+	if(password!=null){
+	    if(password.compareToIgnoreCase("1234")!=0){
+		context.addMessage("Message",
+			    new FacesMessage("Invalid Password."));
+		 return null;
+	    }
+	}
+
 	return NavigationConstants.welcomeScreen;
     }
 
