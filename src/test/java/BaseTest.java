46d43551babce2e13bf0bc2602847a745097fe35
chandrasekhar4u <chandu624@gmail.com>
2013-06-05 17:31:02 +0530

    able to execute prject tests.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -8,20 +8,24 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author CHANDRA
  * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({
-	"classpath:WEB-INF/applicationContextTest.xml",
-	"classpath:WEB-INF/context.xml" })
+@ContextConfiguration(
+	    locations = {"classpath:/WEB-INF/applicationContextTest.xml", "classpath:/WEB-INF/context.xml"}) 
+@TransactionConfiguration
+@Transactional
 public class BaseTest extends AbstractTransactionalJUnit4SpringContextTests {
     
     @Test
     public void dummyTest(){
 	
     }
-
+    
+    
 }
