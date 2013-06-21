c548904341926518ebdbc3946ddb48af7122d6e8
chandrasekhar4u <chandu624@gmail.com>
2013-06-21 10:38:18 +0530

    conflict commit

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-SunlightSolar
+SunlightSolar  
 =============
+Application to Maintain Solar Business work.
 
-Application for Maintain Solar Business work.
+![Build status](https://travis-ci.org/chandrasekhar4u/SunlightSolar.png?branch=master)
+
+
+Modules
+========
+Configuration (user/application)<br/>
+Registration<br/>
+Search and update<br/>
+Reports<rb/>
+
 
 Technologies Used
 ==================
-Spring, JSF, RichFaces, Apache Tomcat,
+Spring, Hibernate, JSF, RichFaces, Apache Tomcat,
+
+## Authors
+
+**Chandra Sekhar**
+
++ [http://twitter.com/chandrasekhar4u](http://twitter.com/chandrasekhar4u)
++ [http://github.com/chandrasekhar4u](http://github.com/chandrasekhar4u)
+
+
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,19 +3,29 @@
  */
 package test.java;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author CHANDRA
  * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({
-	"classpath:META-INF/conf/spring/applicationContextTest.xml",
-	"classpath:META-INF/conf/spring/context.xml" })
+@ContextConfiguration(
+	    locations = {"classpath:/WEB-INF/applicationContextTest.xml", "classpath:/WEB-INF/context.xml"}) 
+@TransactionConfiguration
+@Transactional
 public class BaseTest extends AbstractTransactionalJUnit4SpringContextTests {
-
+    
+    @Test
+    public void dummyTest(){
+	
+    }
+    
+    
 }
