83fbc6a17c18883d80ea67b323df3948b2daa66f
chandrasekhar4u <chandu624@gmail.com>
2014-09-25 02:08:49 +0530

    changed content and removed build folder.

--- src/main/java/controller/LoginBean.java
+++ src/main/java/controller/LoginBean.java
@@ -3,17 +3,6 @@
  */
 package main.java.controller;
 
-import java.io.IOException;
-
-import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import main.hibernate.hbm.UserVO;
 
 import org.apache.myfaces.orchestra.conversation.ConversationBindingEvent;
@@ -30,19 +19,20 @@ public class LoginBean extends BaseController {
     private Integer userId;
 
     private String password;
-    
+
     /**
      * @return the userId
      */
     public Integer getUserId() {
-        return userId;
+	return userId;
     }
 
     /**
-     * @param userId the userId to set
+     * @param userId
+     *            the userId to set
      */
     public void setUserId(Integer userId) {
-        this.userId = userId;
+	this.userId = userId;
     }
 
     /*
@@ -76,38 +66,16 @@ public class LoginBean extends BaseController {
 	return password;
     }
 
-    public String actionLogOut(){
+    public String actionLogOut() {
 	logOut();
 	return null;
     }
-    
-    
 
     /**
      * 
      */
     public static void logOut() {
-
-	FacesContext fc=FacesContext.getCurrentInstance();
-	ExternalContext ec= fc.getExternalContext();
-	HttpServletRequest request= (HttpServletRequest) ec.getRequest();
-	HttpServletResponse response=(HttpServletResponse) ec.getResponse();
-	request.getSession(false).invalidate();
-
-	// re-direct to the application root e.g. /ProdisMoffatPlus
-	// Spring security will automatically forward to the login page as 
-	// the session has now been invalidated
-	try {
-	    String contextRoot = ec.getRequestContextPath();
-	    System.out.println( "redirecting to context root - " + contextRoot );
-	    response.sendRedirect( contextRoot );
-	}
-	catch ( IOException e ) {
-	    System.out.println( "exception redirecting to logon page - " + e.toString() );
-	}
-
-	// tell JSF the response is complete
-	fc.responseComplete();
+	NavigationController.logOut();
     }
 
     /**
@@ -117,42 +85,38 @@ public class LoginBean extends BaseController {
     public void setPassword(String password) {
 	this.password = password;
     }
-    
-    
-    
-    public String addUser(){
+
+    public String addUser() {
 	ConversationUtils.getCurrentBean();
-	UserController userController=UserController.getCurrentInstance();
+	UserController userController = UserController.getCurrentInstance();
 	userController.setUserVO(new UserVO());
 	return NavigationConstants.ADD_USER;
     }
-    
-    
 
     public String actionSubmit() {
-	if(userId!=null && password !=null){
-	UserVO userVO=getUserService().getUserById(userId);
-	System.out.println("UserVO===============>"+userVO);
-	
-	if(userVO!=null){
-	if(userId!=null){
-	    if(userId.compareTo(userVO.getId())!=0){
-		logWarningMessage("Invalid User Name.");
+	if (userId != null && password != null) {
+	    UserVO userVO = getUserService().getUserById(userId);
+	    System.out.println("UserVO===============>" + userVO);
+
+	    if (userVO != null) {
+		if (userId != null) {
+		    if (userId.compareTo(userVO.getId()) != 0) {
+			logWarningMessage("Invalid User Name.");
+			return null;
+		    }
+		}
+		if (password != null) {
+		    if (password.compareToIgnoreCase(userVO.getPassword()) != 0) {
+			logWarningMessage("Invalid Password.");
+			return null;
+		    }
+		}
+	    } else {
+		logWarningMessage("Invalid UserName/Password.");
 		return null;
 	    }
 	}
-	if(password!=null){
-	    if(password.compareToIgnoreCase(userVO.getPassword())!=0){
-		logWarningMessage("Invalid Password.");
-		 return null;
-	    }
-	}
-	}else{
-	    logWarningMessage("Invalid UserName/Password.");
-	 return null;
-	}
-	}
-	
+
 	if (userId == null) {
 	    logWarningMessage("Invalid User Name.");
 	    return null;
@@ -163,8 +127,8 @@ public class LoginBean extends BaseController {
 	    return null;
 
 	}
-	
+
 	return NavigationConstants.welcomeScreen;
     }
-    
+
 }
--- src/main/java/controller/NavigationController.java
+++ src/main/java/controller/NavigationController.java
@@ -3,10 +3,45 @@
  */
 package main.java.controller;
 
+import java.io.IOException;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author CHANDRA
  * 
  */
 public class NavigationController extends BaseController {
+
+    public static void logOut() {
+	// get a handle on the request / response
+	FacesContext context = FacesContext.getCurrentInstance();
+	ExternalContext ec = context.getExternalContext();
+	HttpServletRequest request = (HttpServletRequest) ec.getRequest();
+	HttpServletResponse response = (HttpServletResponse) ec.getResponse();
+
+	// destroy the HTTP session - this will destroy all Apache Orchestra
+	// conversation contexts across all application tabs
+	System.out.println("invalidating session");
+	request.getSession(false).invalidate();
+
+	// re-direct to the application root e.g. /ProdisMoffatPlus
+	// Spring security will automatically forward to the login page as
+	// the session has now been invalidated
+	try {
+	    String contextRoot = ec.getRequestContextPath();
+	    System.out.println("redirecting to context root - " + contextRoot);
+	    response.sendRedirect(contextRoot);
+	} catch (IOException e) {
+	    System.out.println("exception redirecting to logon page - "
+		    + e.toString());
+	}
+
+	// tell JSF the response is complete
+	context.responseComplete();
+    }
 
 }
